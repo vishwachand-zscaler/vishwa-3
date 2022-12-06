812802f8670b42d86510b3ed772a68f307132ab7
vishwachand-zscaler <102510309+vishwachand-zscaler@users.noreply.github.com>
2022-12-06 12:04:49 +0530

    Add files via upload

---
+++ AzureRBAC.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  azure_active_directory_role_based_access_control {
+      managed                 = false
+      azure_rbac_enabled      = false
+    }
+}
