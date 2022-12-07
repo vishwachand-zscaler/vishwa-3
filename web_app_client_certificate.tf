a986e8f09fb35497b871fe98e5d9065606868afb
vishwachand-zscaler <102510309+vishwachand-zscaler@users.noreply.github.com>
2022-12-07 23:43:46 +0530

    Update web_app_client_certificate.tf

--- web_app_client_certificate.tf
+++ web_app_client_certificate.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service" "remediation_example" {
+resource "azurerm_app_service" "remediation_example_1" {
     name                = "example-app-service"
     location            = azurerm_resource_group.example.location
     resource_group_name = azurerm_resource_group.example.name
