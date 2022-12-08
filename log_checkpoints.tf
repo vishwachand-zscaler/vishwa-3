3488199e9bab2f60de2a8365827c5b57702dd8b9
vishwachand-zscaler <102510309+vishwachand-zscaler@users.noreply.github.com>
2022-12-08 12:54:16 +0530

    Update log_checkpoints.tf

--- log_checkpoints.tf
+++ log_checkpoints.tf
@@ -1,4 +1,4 @@
-resource "azurerm_postgresql_configuration" "example" {
+resource "azurerm_postgresql_configuration" "example1" {
   name                = "log_checkpoints"
   resource_group_name = azurerm_resource_group.example.name
   server_name         = azurerm_postgresql_server.example.name
