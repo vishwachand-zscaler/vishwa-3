c66779cb2298de75e24f5ca0e0ad6bca25f4ef2e
vishwachand-zscaler <102510309+vishwachand-zscaler@users.noreply.github.com>
2022-12-13 17:36:52 +0530

    Update ZS-AZURE-00021.tf

--- ZS-AZURE-00021.tf
+++ ZS-AZURE-00021.tf
@@ -1,4 +1,4 @@
-resource "azurerm_function_app" "remediation_example" {
+resource "azurerm_function_app" "21_example" {
   name                = "example-function-app"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
