f350dc2331b10f83639ed67c78633f055a38f9eb
vishwachand-zscaler <102510309+vishwachand-zscaler@users.noreply.github.com>
2022-12-13 17:31:15 +0530

    Update and rename app_service_auth_disabled to ZS-AZURE-00021.tf

--- ZS-AZURE-00021.tf
+++ ZS-AZURE-00021.tf
@@ -3,6 +3,7 @@ esource "azurerm_function_app" "remediation_example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_function_app_plan.example.id
+  
 
   auth_settings {
     enabled = false
