22f5b99c338aeb13294addb25a7ebc213262fe66
vishwachand-zscaler <102510309+vishwachand-zscaler@users.noreply.github.com>
2022-12-14 12:24:48 +0530

    Delete log_disconnections.tf

--- log_disconnections.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_postgresql_configuration" "example4" {
-  name                = "log_disconnections"
-  resource_group_name = azurerm_resource_group.example.name
-  server_name         = azurerm_postgresql_server.example.name
-  value               = "off"
-}
