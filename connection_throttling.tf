23995129c55e1c0e148556d2679a1461851b59bd
vishwachand-zscaler <102510309+vishwachand-zscaler@users.noreply.github.com>
2022-12-14 12:03:28 +0530

    Delete connection_throttling.tf

--- connection_throttling.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_postgresql_configuration" "example" {
-  name                = "connection_throttling"
-  resource_group_name = azurerm_resource_group.example.name
-  server_name         = azurerm_postgresql_server.example.name
-  value               = "off"
-}
