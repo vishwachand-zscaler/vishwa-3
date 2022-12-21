fb0e878e6c5140173e37066130870786b02ad607
vishwachand-zscaler <102510309+vishwachand-zscaler@users.noreply.github.com>
2022-12-22 00:03:47 +0530

    Delete log_connections.tf

--- log_connections.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_postgresql_configuration" "example3" {
-  name                = "log_connections"
-  resource_group_name = azurerm_resource_group.example.name
-  server_name         = azurerm_postgresql_server.example.name
-  value               = "off"
-}
