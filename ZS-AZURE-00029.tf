1dd35aa02fd7181a98bbc41ec3280da57e43134b
vishwachand-zscaler <102510309+vishwachand-zscaler@users.noreply.github.com>
2022-12-22 11:37:13 +0530

    Create ZS-AZURE-00029.tf

---
+++ ZS-AZURE-00029.tf
@@ -0,0 +1,10 @@
+resource "azurerm_app_service" "remediation_example" {
+    name                = "example-app-service"
+    location            = azurerm_resource_group.example.location
+    resource_group_name = azurerm_resource_group.example.name
+    app_service_plan_id = azurerm_app_service_plan.example.id
+
+    site_config {
+        http2_enabled = false
+    }
+}
